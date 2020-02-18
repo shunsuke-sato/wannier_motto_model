6226bb184cae89956ed4beec81f73e0f43b077cf
Shunsuke Sato <shunsuke.sato26@gmail.com>
2020-02-18 13:27:54 +0100

    Added laser initialization.

--- main.f90
+++ main.f90
@@ -18,6 +18,7 @@ module global_variables
   real(8) :: eps_cutoff
   real(8) :: mu_mass
   real(8) :: v0_coulomb
+  real(8) :: eps_gap
   real(8),allocatable :: kx0(:)
   real(8),allocatable :: ham0(:,:), ham_t(:,:)
   real(8),allocatable :: eigval_ham0(:),eigvec_ham0(:,:)
@@ -25,7 +26,18 @@ module global_variables
 
 ! time propagation
   real(8) :: Tprop, dt
+  real(8) :: Tprop_fs
   integer :: nt
+
+! laser
+  real(8),allocatable :: Apump(:),Eprobe(:)
+  real(8) :: Tpump_fs, Tpump
+  real(8) :: omega_pump_ev, omega_pump
+  real(8) :: Tprobe_fs, Tprobe
+  real(8) :: omega_probe_ev, omega_probe
+  real(8) :: Tdelay_fs, Tdelay
+  real(8) :: A0_pump, E0_probe
+  
 
 end module global_variables
 !-------------------------------------------------------------------------------
@@ -47,11 +59,31 @@ subroutine input
   eps_cutoff = 15d0*ev
   mu_mass = 1d0
   nkx = 256+1
+
+  eps_gap = 40d0*ev
   v0_coulomb = 1d0
+
+! lasers
+  Tpump_fs = 20d0
+  Tprobe_fs = 1d0
+  omega_pump_ev = 1.55d0
+  omega_probe_ev = 40d0
+  Tdelay_fs = 0d0
+
+  Tprop_fs = 100d0
+  dt = 0.08d0
+
+
+  A0_pump = 1d-2
+  E0_probe = 1d-4
+
 
   kx_max = sqrt(2d0*mu_mass*eps_cutoff)
   dkx = 2d0*kx_max/nkx
   
+  
+  Tprop = Tprop_fs*fs
+  nt = aint(Tprop/dt)+1
 
 end subroutine input
 !-------------------------------------------------------------------------------
@@ -82,7 +114,7 @@ subroutine preparation
 
   ham0 = 0d0
   do ikx = 1, nkx
-    ham0(ikx,ikx) = 0.5d0*kx0(ikx)**2/mu_mass
+    ham0(ikx,ikx) = eps_gap + 0.5d0*kx0(ikx)**2/mu_mass
     do ikx2 = ikx+1,nkx
       kdist = abs(kx0(ikx) -kx0(ikx2))
       ham0(ikx,ikx2) = -2d0*v0_coulomb*(dkx/(2d0*pi))*besk0(kdist)
@@ -119,10 +151,58 @@ subroutine time_propagation
   use global_variables
   implicit none
 
+
+  call init_laser
   
 
 end subroutine time_propagation
 !-------------------------------------------------------------------------------
+subroutine init_laser
+  use global_variables
+  implicit none
+  integer :: it
+  real(8) :: tt, xx
+  real(8) :: t_offset
+
+  allocate(Apump(-1:nt+1), Eprobe(-1:nt+1))
+
+  Tpump = Tpump_fs*fs
+  omega_pump = omega_pump_ev*ev
+  Tprobe = Tprobe_fs*fs
+  omega_probe = omega_probe_ev*ev
+  Tdelay = Tdelay_fs*fs
+
+  t_offset = min(-0.5d0*Tpump, -0.5d0*Tprobe + Tdelay)
+
+  
+  Apump = 0d0
+  Eprobe = 0d0
+
+
+  do it = 0, nt+1
+    tt = dt*it
+! A-pump
+    xx = tt + t_offset
+    if(abs(xx)<=0.5d0*Tpump)then
+      Apump(it) = A0_pump*cos(omega_pump*xx) *cos(pi*xx/Tpump)**2
+    end if
+
+! Eprobe
+    xx = tt-Tdelay + t_offset
+    if(abs(xx)<=0.5d0*Tprobe)then
+      Eprobe(it) = E0_probe*sin(omega_probe*xx)*cos(pi*xx/Tprobe)**4
+    end if
+
+  end do
+
+  open(20,file='lasers.out')
+  do it = 0,nt+1
+    write(20,"(999e26.16e3)")dt*it,Apump(it),Eprobe(it)
+  end do
+  close(20)
+  
+
+end subroutine init_laser
 !-------------------------------------------------------------------------------
 !-------------------------------------------------------------------------------
 !-------------------------------------------------------------------------------
