bed759d54612595218e28d05988d6caeadef376f
Shunsuke Sato <shunsuke.sato26@gmail.com>
2020-02-21 13:10:13 +0100

    Modified sigma.f90

--- sigma.f90
+++ sigma.f90
@@ -5,7 +5,7 @@ program main
   real(8),parameter :: ev = 1d0/27.2114d0
   real(8),parameter :: wi = 45d0*ev, wf = 65d0*ev
   real(8),parameter :: dw = (wf-wi)/nw
-  real(8),parameter :: gamma = 0.1d0*ev
+  real(8),parameter :: gamma = 0.2d0*ev
   complex(8),parameter :: zi = (0d0, 1d0)
   real(8) :: tt(0:nt),Apump(0:Nt),Eprobe(0:nt),jt(0:nt)
   real(8) :: ww, dt
@@ -27,18 +27,20 @@ program main
     end if
   end do
 
+  it_ini = 0
+
   open(30,file="zsigma.out")
   do iw = 0, nw
     ww = wi + dw*iw
     zjw = 0d0
-    zEw = 0d0
+!    zEw = 0d0
     do it = it_ini, nt
       zfact = exp(zI*ww*tt(it)-gamma*(tt(it)-tt(it_ini)))
       zjw = zjw + zfact*jt(it)
-      zEw = zEw + zfact*Eprobe(it)
+!      zEw = zEw + zfact*Eprobe(it)
     end do
     zjw = zjw*dt
-    zEw = zEw*dt
+    zEw = 1d0*dt
     zsigma = zjw/zEw
     write(30,"(999e26.16e3)")ww,zsigma, abs(zEw)
 
